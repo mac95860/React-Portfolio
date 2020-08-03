dad586953333825476a92aa9ef1fe041d5dfc594
Michael Cervantes <mac95860@hotmail.com>
2020-08-02 21:39:49 -0500

    style prop

--- src/components/Jumbotron/Jumbotron.js
+++ src/components/Jumbotron/Jumbotron.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./jumbotron.css";
-import "../../../public/images"
+
 
 function Jumbotron() {
     return (
@@ -8,7 +8,7 @@ function Jumbotron() {
             <div class="container-fluid vh-100">
                 <div class="container text-center">
                     <h1 class="display-1">About Me</h1>
-                    <img src="../../../public/images/me.jpg" alt="portrait" class="rounded-circle shadow"/>
+                    <img src="../../images/me.jpg" alt="portrait" class="rounded-circle shadow"/>
                     <p class="lead  rounded vw-75"> I am a web developer familiar with several different programming languages and frameworks including
                         JavaScript, JQuery, Node.js, Express.js, Passport.js, and Handlebars.js. Also, I have worked with styling languages such as CSS, Bootstrap4,
                         and Bulma. In the projects I have worked on I have worked both with MySQL and NoSQL databases such as MongoDB. Other tools I have used are Test
